725ea1454e03d93fddd1b0f4dc219fee815b7dc0
Recap24 <mostafezurrahman400@gmail.com>
2026-03-05 16:38:07 +0600

    Exercise on variable

--- first_program.py
+++ first_program.py
@@ -1,4 +1,82 @@
-print("Hello, World!")
-print("Welcome to Python programming.")
-print("This is my first program.")  
-print("I am excited to learn more about Python!")
+#      Variables
+
+"""Problem -01 :
+Create and Print Variables
+
+Create three variables:
+name
+age
+country
+
+Assign your own values and print them in one line.
+
+"""
+
+
+name = "Rahim"
+age = 20 
+country = "Bangladesh"
+
+
+print("My name is", name, "I am", age, "years old and I live in", country)
+
+
+"""
+problem-02: Add Two Numbers
+
+Create two variables a and b.
+Assign numbers to them and print their sum.
+
+Output: 15
+
+"""
+
+
+a = 10 
+b = 5 
+
+print(a + b)
+
+
+
+"""
+Problem-03: Swap Two Variables
+
+Create two variables x and y.
+Swap their values without losing the original numbers.
+"""
+
+x = 20 
+y = 10
+
+# after swap 
+
+x = 10 
+y = 20 
+
+"""
+Problem-04 : Calculate Rectangle Area
+
+Create two variables:
+
+length
+
+width
+
+Calculate and print the area of a rectangle.
+"""
+length = 20 
+width = 5
+area = (length * width)
+print(area)
+
+
+"""Problem-05: Convert Temperature
+
+Create a variable celsius.
+Convert it into Fahrenheit using the formula.
+"""
+celsius = 25
+fahrenheit = (celsius * 9/5) + 32
+
+print(fahrenheit)
